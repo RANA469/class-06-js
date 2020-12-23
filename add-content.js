521b6d7379d82ac7dcfc623b974409e115786c91
RANA469 <ranashelbaya.98@yahoo.com>
2020-12-23 15:23:33 +0200

    add js

--- add-content.js
+++ add-content.js
@@ -1,1 +1,19 @@
+console.log('hello world');
+var pi=3.14;
+var name="rana";
+var grade=0;
+grade=5
+grade=grade+7;
+var firstname='rana';
+var y=9;
+var name= prompt('please enter your name');
+alert('hi user!welcome to my website');
+var time=1;
+if('time<7>'){
+    message='true'
+}else{
+message='false'
+}
+var time=prompt('what time is it ')
+
 
